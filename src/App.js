b7d79c45f5334a9fcec81ffbdbd857c7c7d2df01
Elliott Chong <elliottchong16@gmail.com>
2021-11-02 20:06:20 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
+import React from "react";
+
 function App() {
-  return (
-    <>
-      <h1
-        style={{
-          position: "absolute",
-          left: "100vh",
-          transform: "translateX(-50%)",
-          fontFamily: "poppins",
-        }}
-      >
-        hi andy
-      </h1>
-      <div class="div">
-        <span class="container">
-          <a href="#">
-            <span class="yes">Hello World</span>
-          </a>
-        </span>
-      </div>
-    </>
-  );
+  return <div className="container">My Landing Page</div>;
 }
 
 export default App;
