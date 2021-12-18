c23bddb3a87f5f1faf0ede10c9568234785cd872
Elliott <elliottchong16@gmail.com>
2021-12-18 16:54:44 +0800

    caeser

---
+++ src/components/Caeser.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Controller, Scene } from "react-scrollmagic";
+import code from "../images/caeser-code.png";
+import fish from "../images/caeser-code.png";
+import Button from "./Button";
+
+const Caeser = () => {
+  return (
+    <>
+      <Controller>
+        <Scene classToggle="fade-in" indicators={true} triggerHook={0.8}>
+          <div id="caeser-wrapper">
+            <h1
+              id="caeser-text"
+              className="space-elle ml-5 text-2xl lg:ml-12 lg:text-5xl font-mono relative text-terminalGreen"
+            >
+              This year, I started getting even more into programming, taking
+              CS50, Harvard University's Intro to Computer Science course
+              online.
+            </h1>
+          </div>
+        </Scene>
+        <Scene
+          classToggle="fade-in"
+          triggerElement="#caeser-wrapper"
+          triggerHook={0.7}
+        >
+          <div id="caeser-img-wrapper">
+            <div className="caeser-img">
+              <img src={code} alt="caeser code" className=" lg:ml-0" />
+              <p className="description font-mono text-xl text-center">
+                My first caesar cipher script written in C
+              </p>
+              <Button id="caeser-button">
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://replit.com/@ElliottChong/Caesar-Cipher#main.c"
+                >
+                  Link to Code
+                </a>
+              </Button>
+            </div>
+            <div className="caeser-img ">
+              <iframe
+                src="https://replit.com/@ElliottChong/Caesar-Cipher?embed=true"
+                frameborder="0"
+              ></iframe>
+            </div>
+          </div>
+        </Scene>
+      </Controller>
+    </>
+  );
+};
+
+export default Caeser;
---
+++ src/components/Minecraft.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Controller, Scene } from "react-scrollmagic";
+import code from "../images/minecraft-code.png";
+import fish from "../images/minecraft-fish.png";
+const Minecraft = () => {
+  return (
+    <>
+      <Controller>
+        <Scene classToggle="fade-in" indicators={true} triggerHook={0.8}>
+          <div id="minecraft-wrapper">
+            <h1
+              id="minecraft-text"
+              className="space-elle ml-5 text-2xl lg:ml-12 lg:text-5xl font-mono relative text-terminalGreen"
+            >
+              It was around Secondary 3 where I started teaching myself Python
+              through online tutorials, building out many small projects along
+              the way..
+            </h1>
+          </div>
+        </Scene>
+        <Scene
+          classToggle="fade-in"
+          triggerElement="#minecraft-wrapper"
+          triggerHook={0.7}
+        >
+          <div id="minecraft-img-wrapper">
+            <div className="minecraft-img lg:ml-12">
+              <img src={code} alt="minecraft code" className="ml-10 lg:ml-0" />
+              <p className="description font-mono text-xl text-center">
+                a simple script I wrote to automate fishing in Minecraft
+              </p>
+            </div>
+            <div className="minecraft-img">
+              <img src={fish} alt="minecraft code" className="ml-5 lg:ml-12" />
+            </div>
+          </div>
+        </Scene>
+      </Controller>
+    </>
+  );
+};
+
+export default Minecraft;
